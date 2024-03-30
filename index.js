7a54a4f9d034401c46f389dbdca39a4e72e35f69
victormuthiora <victormuthiora617@gmail.com>
2024-03-30 13:19:16 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// code your solution here!
+function superbowlWin(record) {
+    let win = record.find(function(game) {
+        return game.result === "W";
+    });
+    return win ? win.year : undefined;
+}
+
